b0fec08b6ec065c24a8b78d418bea4d22692f349
tonyketcham <ketcham@pnw.edu>
2020-06-07 11:41:27 -0500

    Simplified canvas component with Svelte actions

--- src/PaperCanvas.svelte
+++ src/PaperCanvas.svelte
@@ -2,27 +2,8 @@
   export let resize = true;
 
   import { paper } from "paper";
+  import newPaperCanvas from "./paperCanvas.js";
   import { onMount } from "svelte";
-
-  function newPaperCanvas() {
-    // Get a reference to the canvas object
-    var canvas = document.getElementById("paperCanvas");
-    // Create an empty project and a view for the canvas:
-    paper.setup(canvas);
-    // Create a Paper.js Path to draw a line into it:
-    var path = new paper.Path();
-    // Give the stroke a color
-    path.strokeColor = "black";
-    var start = new paper.Point(100, 100);
-    // Move to start and draw a line from there
-    path.moveTo(start);
-    // Note that the plus operator on Point objects does not work
-    // in JavaScript. Instead, we need to call the add() function:
-    path.lineTo(start.add([200, -50]));
-    // Draw the view now:
-    paper.view.draw();
-  }
-  onMount(newPaperCanvas);
 </script>
 
 <style>
@@ -34,7 +15,7 @@
 </style>
 
 {#if !resize}
-  <canvas id="paperCanvas" />
+  <canvas use:newPaperCanvas />
 {:else}
-  <canvas id="paperCanvas" resize />
+  <canvas use:newPaperCanvas resize />
 {/if}
